e1974080b7509c9983ab6de1ef1d638dd46a698e
Karen Bemis <bemis@marine.rutgers.edu>
2026-02-06 17:08:40 -0500

    still completing read code

--- read_kmall.m
+++ read_kmall.m
@@ -76,3 +76,26 @@ TT=zeros(ckNrx(1),maxWCSampIdx,Ndgm);
 XX=zeros(ckNrx(1),maxWCSampIdx,Ndgm);
 xBottom=zeros(Ndgm,ckNrx(1));
 dTime=zeros(Ndgm,1);
+
+
+% loop over datagrams
+%
+%        TxRad = TxBeamWidth(cenSec)*pi/180*recieveAngle;
+%        Width = 2*ones(length(TxRad),length(range)).*range.*(sin(TxRad./2));
+%        BeamArea = Length.*Width;
+%
+%        Tau = 3500/1e5;
+%        Sv=zeros(size(TS));
+%        for ii = 1:size(TS,2)
+%            Vol_log = 10*log10(BeamArea(:,ii)*Tau*SoundSpeed/2);
+%            Sv(:,ii) = TS(:,ii) - Vol_log;
+%        end
+%
+%    % store for s
+%    YY(:,:,pingidx)=y(:,1:maxWCSampIdx);
+%    ZZ(:,:,pingidx)=z(:,1:maxWCSampIdx);
+%    SV(:,:,pingidx)=Sv(:,1:maxWCSampIdx);
+%    TT(:,:,pingidx)=TS(:,1:maxWCSampIdx);
+%    XX(:,:,pingidx)=cartspeed*elapsedsec*ones(256,maxWCSampIdx);
+%    xBottom(pingidx,:)=cartspeed*elapsedsec*ones(1,256);
+%    dTime(pingidx)=elapsedsec;
