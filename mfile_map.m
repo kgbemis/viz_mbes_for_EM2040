b8fc5b0622738f7d2369a4fb8437486925fdd377
Karen Bemis <bemis@marine.rutgers.edu>
2026-09-23 16:52:36 -0400

    adding a guide to the m-files

---
+++ mfile_map.m
@@ -0,0 +1,19 @@
+% m-file map!
+
+% readEM_kmall.m - original m-file for processing data
+%   calls read_bin_kmall.m
+%   saves outmatfile - which has the water column data structure
+%         outvizfile - which has grids for visualization
+%         outallstruct - which saves the basic input metadata
+
+% read_bin_kmall.m - reads the binary data in the .kmwcd file
+%   calls n/a
+%   saves n/a
+
+% sum_viz.m
+% 
+check_sampinfo.m       parse_install_txt.m    read_kmall.m           
+check_sonar_param.m    parse_metadata.m                     
+detect_bottom.m        process_file_series.m  
+fill_grid.m                    
+get_kmwcd_freq.m       
